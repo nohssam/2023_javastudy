c14026aef53151f9f81363b2e8934164971462f6
nohssam73 <nohssam73@gmail.com>
2023-06-13 17:57:44 +0900

    20230613

---
+++ bin/ERMaster/test01.sql
@@ -0,0 +1,53 @@
+
+/* Drop Tables */
+
+--DROP TABLE orders CASCADE CONSTRAINTS;
+--DROP TABLE customer CASCADE CONSTRAINTS;
+
+
+
+
+/* Create Tables */
+
+CREATE TABLE customer2
+(
+	custid number NOT NULL,
+	name varchar2(50),
+	address varchar2(50 char),
+	phone varchar2(50),
+	PRIMARY KEY (custid)
+);
+
+
+CREATE TABLE orders2
+(
+	orderid number NOT NULL,
+	bookid number,
+	salprice number,
+	orderdate date,
+	custid number NOT NULL,
+	PRIMARY KEY (orderid)
+);
+
+
+
+/* Create Foreign Keys */
+
+ALTER TABLE orders
+	ADD FOREIGN KEY (custid)
+	REFERENCES customer (custid)
+;
+
+select * from CUSTOMER;
+
+
+
+
+
+
+
+
+
+
+
+
